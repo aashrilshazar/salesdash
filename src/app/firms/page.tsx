99aa2f2d41f71cc85745d20a7bab9c3ea07cff0f
Aashril  Shazar <aashrilshazar@Bens-M2-Air.localdomain>
2025-07-01 12:30:46 -0400

    make /firms dynamic

--- src/app/firms/page.tsx
+++ src/app/firms/page.tsx
@@ -1,4 +1,6 @@
 // src/app/firms/page.tsx
+// force this page to always render dynamically (no SSG/cache)
+export const dynamic = 'force-dynamic'
 import FirmsTable from './FirmsTable';
 import { fetchFirms } from '../lib/googleSheets';
 
